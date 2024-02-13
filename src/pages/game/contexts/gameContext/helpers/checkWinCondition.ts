c3ee9a27315cdcae8c4e70504cf74f07bf83c5f3
Kapustian2 <123888044+Kapustian2@users.noreply.github.com>
2024-02-13 17:06:23 +0600

    implement second win condition

--- src/pages/game/contexts/gameContext/helpers/checkWinCondition.ts
+++ src/pages/game/contexts/gameContext/helpers/checkWinCondition.ts
@@ -8,6 +8,10 @@ import { type GameState } from "../reducers/gameReducer";
 export const checkWinCondition = (gameState: GameState) => {
   const field = gameState.gameField.field.flat();
   const openCells = field.filter((cell) => !cell.isMined && cell.isOpen).length;
+  const markCells = field.filter((cell) => cell.isMined && cell.mark).length;
 
-  return openCells === field.length - gameState.gameField.minesCount;
+  return (
+    openCells === field.length - gameState.gameField.minesCount ||
+    markCells === field.length - gameState.gameField.minesCount
+  );
 };
